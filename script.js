67e7b54ff9f997f09312d53af564865940e64795
hctrgnzlz <hectorgzz89@gmail.com>
2020-06-11 17:36:56 -0400

    added new api/html/css

--- script.js
+++ script.js
@@ -1,51 +1,41 @@
 // Create app namespace to hold all methods
 const imageApp = {};
-
-imageApp.key = '563492ad6f9170000100000118fca25d2bb94e91be6be40c94ac26ec';
-imageApp.url = 'https://api.pexels.com/v1/search';
-
+imageApp.key = "zjqyoonlwHaTOJEZCl_pl8SAQNAWIlpHv0ISu-jfJxE";
+imageApp.url = "https://api.unsplash.com/photos/random";
 imageApp.getImage = function () {
-
-    $.ajax({
-        url: imageApp.url,
-        method: 'GET',
-        dataType: 'json',
-        data: {
-            key: imageApp.key,
-            format: 'json',
-            query: 'happy' // Will request a search term
-        },
-        headers: {
-            'Authorization': imageApp.key
-        }
-        // Response is returning the function above AFTER ajax has completed
-    }).then(function (response) {
-        imageApp.displayImage(response.photos);
-        // console.log(response.photos);
-    });
-
+  $.ajax({
+    url: imageApp.url,
+    method: "GET",
+    dataType: "json",
+    data: {
+      client_id: imageApp.key,
+      format: "json",
+      count: "6",
+      query: "travel", // Will request a search term
+    },
+    // Response is returning the function above AFTER ajax has completed
+  }).then(function (response) {
+    imageApp.displayImage(response);
+    console.log(response);
+  });
 };
-
 // passing in from the "response.photos" to imageArray
-imageApp.displayImage = function(imageArray) {
-    imageArray.forEach(function(image) {
-        console.log(image);
-        
-        const photo = $('<img>').addClass('gallery-img').attr('src', image.src.portrait).attr('alt', `${image.photographer}'s photo`);
-        const photography = $('<p>').addClass('photographer').text(image.photographer);
-        const imagePiece = $('<div>').addClass('piece').append(photo, photography);
-
-        $('.gallery-grid').append(imagePiece);
-    });
+imageApp.displayImage = function (imageArray) {
+  imageArray.forEach(function (image) {
+    console.log(image);
+    const photo = $("<img>")
+      .addClass("gallery-img")
+      .attr("src", image.urls.regular)
+      .attr("alt", image.alt_description);
+    const photography = $("<p>").addClass("photographer").text(image.user.name);
+    const imagePiece = $("<div>").addClass("piece").append(photo, photography);
+    $(".gallery-grid").append(imagePiece);
+  });
 };
-
 // Start app
-imageApp.init = function() {
-    imageApp.getImage();
+imageApp.init = function () {
+  imageApp.getImage();
 };
-
 $(function () {
-
-    imageApp.init();
-
+  imageApp.init();
 });
